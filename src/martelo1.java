b6a4fedc4b867c65869a79f742bad2638158f0e2
Markcodeswor <113196496+marcos-de-santana@users.noreply.github.com>
2023-04-21 08:25:38 -0300

    Initial commit

---
+++ src/martelo1.java
@@ -0,0 +1,20 @@
+public class martelo1 {
+   
+        //atributos
+
+        String marca;
+        String tamanho;
+        String modelo;
+
+        int duracao;
+
+        //métodos
+
+        void martelar(){
+
+        }
+        void desapregar(){
+
+        }
+
+    }
